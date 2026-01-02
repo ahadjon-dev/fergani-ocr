dbae5dc7c6321916d3c6c9062a0ed614061d0fcc
Ahadjon <ahadjon.ubaydullayev@email.com>
2026-01-03 02:58:00 +0500

    allowed hosts

--- fergani/settings.py
+++ fergani/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-i)jjs8pk2gwley+dhnil1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1,healthcheck.railway.app").split(",")
+ALLOWED_HOSTS = os.environ.get(
+    "ALLOWED_HOSTS", "localhost,127.0.0.1,healthcheck.railway.app,web-production-8677.up.railway.app,.railway.app"
+).split(",")
 
 # CSRF trusted origins for production
 CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",") if os.environ.get("CSRF_TRUSTED_ORIGINS") else []
